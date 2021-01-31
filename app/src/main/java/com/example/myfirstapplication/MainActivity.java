f82e0a55e86c926ccdaf214d9f0c698c6240bebb
pyz6666 <893758346@qq.com>
2021-01-31 18:40:04 +0800

    计算进布局点击事件以及通过selector进行颜色的变化

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.qq_layout);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ButtonC(View view) {
+        Toast.makeText(this,"1111",Toast.LENGTH_LONG);
     }
 }
